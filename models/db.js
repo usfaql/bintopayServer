980097ce1208dfc8cfa1ef8cae563c4a42d664e3
usfaql <146733467+usfaql@users.noreply.github.com>
2024-02-01 03:40:02 +0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-
-mongoose.connect(process.env.DB_URI,{
-    tlsAllowInvalidHostnames: true,
-    tlsAllowInvalidCertificates: true,
-  }).then((result) => {
-    console.log("DB Ready To Use");
-}).catch((err) => {
-    console.log(err);
-});
+async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
